9745dba0716545b3c12e00f831c6ea647e4ebf09
ivor grlas <ivor.grlas@actual-it.net>
2019-08-29 10:01:07 +0200

    feat: SecurityStampStore implemented and SecurityStamp added to UserBase

---
+++ CoreSharp.Identity/UserStore.SecurityStampStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using NHibernate.Linq;
+
+namespace CoreSharp.Identity
+{
+    public partial class UserStore<TUser, TRole, TOrganization, TUserRole, TOrganizationRole, TRolePermission, TPermission, TClaim> : IUserSecurityStampStore<TUser>
+    {
+
+        public Task SetSecurityStampAsync(TUser user, string stamp, CancellationToken cancellationToken)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user));
+            }
+
+            user.SecurityStamp = stamp;
+
+            return Task.CompletedTask;
+        }
+
+        public Task<string> GetSecurityStampAsync(TUser user, CancellationToken cancellationToken)
+        {
+
+            return _session.Query<TUser>()
+                .Where(x => x.Id == user.Id)
+                .Select(x => x.SecurityStamp)
+                .SingleOrDefaultAsync(cancellationToken);
+        }
+    }
+}
+
