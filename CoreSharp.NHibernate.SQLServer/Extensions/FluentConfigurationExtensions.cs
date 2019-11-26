97df4f2f317288980a6525d3fd27b1251fa92b05
cime <cime3d@gmail.com>
2019-11-26 09:02:27 +0100

    feat: CreateSQLServerSchemas extension

--- CoreSharp.NHibernate.SQLServer/Extensions/FluentConfigurationExtensions.cs
+++ CoreSharp.NHibernate.SQLServer/Extensions/FluentConfigurationExtensions.cs
@@ -1,5 +1,7 @@
 using CoreSharp.NHibernate.Conventions.Mssql;
+using CoreSharp.NHibernate.Extensions;
 using FluentNHibernate.Cfg;
+using System.Linq;
 
 namespace CoreSharp.NHibernate.SQLServer.Extensions
 {
@@ -25,6 +27,25 @@ namespace CoreSharp.NHibernate.SQLServer.Extensions
                 foreach (var persistenceModel in m.AutoMappings)
                 {
                     persistenceModel.Conventions.AddFromAssemblyOf<MssqlHiLoIdConvention>();
+                }
+            });
+        }
+
+        public static FluentConfiguration CreateSQLServerSchemas(this FluentConfiguration fluentConfiguration)
+        {
+            return fluentConfiguration.ExposeDbCommand((command, cfg) =>
+            {
+                var schemas = cfg.ClassMappings.Select(x => x.Table.Schema).Where(x => !string.IsNullOrEmpty(x))
+                    .Distinct()
+                    .Select(x => cfg.NamingStrategy.TableName(x))
+                    .ToList();
+
+                foreach (var schema in schemas)
+                {
+                    command.CommandText = $@"IF NOT EXISTS(SELECT * FROM sys.schemas WHERE name = N'{schema}') 
+                        EXEC('CREATE SCHEMA [{schema}]');
+                    GO";
+                    command.ExecuteNonQuery();
                 }
             });
         }
