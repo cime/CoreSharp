3849fabcdda06b62abc19940330604b14e5b948e
maca88 <bostjan.markezic@siol.net>
2020-02-10 13:49:34 +0100

    Simplify names

--- CoreSharp.Validation/ValidationRuleSet.cs
+++ CoreSharp.Validation/ValidationRuleSet.cs
@@ -2,23 +2,20 @@
 {
     public class ValidationRuleSet
     {
+        private const string InsertRule = "Insert";
+        private const string UpdateRule = "Update";
+
         public const string Default = "default";
-        public const string Delete = "Delete";
-        public const string Insert = "Insert";
-        public const string Update = "Update";
+
         public const string Attribute = "Attribute";
 
-        public static string[] AttributeInsert => Combine(Attribute, Insert);
+        public const string Delete = "Delete";
 
-        public static string[] AttributeInsertDefault => Combine(Attribute, Insert, Default);
+        public static string[] Insert => Combine(Attribute, InsertRule, Default);
 
-        public static string[] AttributeInsertUpdate => Combine(Attribute, Insert, Update);
+        public static string[] InsertUpdate => Combine(Attribute, InsertRule, UpdateRule, Default);
 
-        public static string[] AttributeInsertUpdateDefault => Combine(Attribute, Insert, Update, Default);
-
-        public static string[] AttributeUpdate => Combine(Attribute, Update);
-
-        public static string[] AttributeUpdateDefault => Combine(Attribute, Update, Default);
+        public static string[] Update => Combine(Attribute, UpdateRule, Default);
 
         public static string[] Combine(params string[] rules)
         {
