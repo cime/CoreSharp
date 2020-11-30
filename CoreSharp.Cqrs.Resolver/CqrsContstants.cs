5586daa795ee8bec9af8c2fb1fc4c9233c1c673f
jernejr <jernej.rozac@actual-it.si>
2020-11-30 10:59:17 +0100

    Cqrs resolver.

---
+++ CoreSharp.Cqrs.Resolver/CqrsContstants.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreSharp.Cqrs.Command;
+using CoreSharp.Cqrs.Query;
+
+namespace CoreSharp.Cqrs.Resolver
+{
+    internal static class CqrsContstants
+    {
+        public static List<Type> CqrsInterfaces = new List<Type> {
+            typeof(IAsyncQuery<>),
+            typeof(IQuery<>),
+            typeof(IAsyncCommand<>),
+            typeof(ICommand<>),
+            typeof(IAsyncCommand),
+            typeof(ICommand)
+        };
+
+        public static List<Type> CqrsAsyncInterfaces = new List<Type> {
+            typeof(IAsyncQuery<>),
+            typeof(IAsyncCommand<>),
+            typeof(IAsyncCommand),
+        };
+
+        public static List<Type> CqrsQueryInterfaces = new List<Type> {
+            typeof(IAsyncQuery<>),
+            typeof(IQuery<>),
+        };
+
+        public static List<Type> CqrsCommandInterfaces = new List<Type> {
+            typeof(IAsyncCommand<>),
+            typeof(ICommand<>),
+            typeof(IAsyncCommand),
+            typeof(ICommand)
+        };
+    }
+}
---
+++ CoreSharp.Cqrs.Resolver/CqrsInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CoreSharp.Cqrs.Resolver
+{
+    public class CqrsInfo
+    {
+
+        public CqrsInfo(Type reqType, string serviceName, string methodName, string formatter, bool isQuery, bool isCommand, bool isAsync, Type rspType)
+        {
+            ReqType = reqType;
+            RspType = rspType;
+            IsAsync = isAsync;
+            IsQuery = isQuery;
+            IsCommand = isCommand;
+            ServiceName = serviceName;
+            MethodName = methodName;
+            Formatter = formatter;
+        }
+
+        public Type ReqType { get; }
+
+        public Type RspType { get; }
+
+        public bool IsAsync { get; }
+
+        public bool IsQuery { get; }
+
+        public bool IsCommand { get; }
+
+        public string ServiceName { get; }
+
+        public string MethodName { get; }
+
+        public string Formatter { get; }
+    }
+}
