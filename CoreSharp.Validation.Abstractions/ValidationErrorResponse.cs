7029a23864d2bc9ed12757695f0f34cb989acd9c
jernejr <jernej.rozac@actual-it.si>
2020-11-30 11:02:00 +0100

    Validation error models.

---
+++ CoreSharp.Validation.Abstractions/ValidationErrorResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace CoreSharp.Validation
+{
+    public class ValidationErrorResponse
+    {
+        public enum ErrorSeverity
+        {
+            Error = 0,
+            Warning = 1,
+            Info = 2
+        }
+
+        public class ValidationError
+        {
+            public string ErrorName { get; set; }
+
+            public string EntityTypeName { get; set; }
+
+            public object[] KeyValues { get; set; }
+
+            public string PropertyName { get; set; }
+
+            public string ErrorMessage { get; set; }
+
+            public object AttemptedValue { get; set; }
+
+            public string ErrorCode { get; set; }
+
+            public ErrorSeverity Severity { get; set; }
+        }
+
+        public string ErrorMessage { get; set; }
+
+        public ValidationError[] Errors { get; set; }
+    }
+}
