45fdecbcaa39f7abde55d6be911e49da70ae1e5b
Andrej Cimperšek <andrej.cimpersek@actual-it.si>
2020-12-11 15:32:15 +0100

    feat: custom automaping alternation - injected overrides feat: MapCitext extension method

--- CoreSharp.NHibernate/CustomAutoPersistenceModel.cs
+++ CoreSharp.NHibernate/CustomAutoPersistenceModel.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using CoreSharp.Cqrs.Events;
 using CoreSharp.NHibernate.Events;
 using CoreSharp.NHibernate.Visitors;
 using FluentNHibernate.Automapping;
 using FluentNHibernate.MappingModel;
+using FluentNHibernate.Utils;
+using FluentNHibernate.Utils.Reflection;
 using SimpleInjector;
 
 namespace CoreSharp.NHibernate
@@ -48,6 +51,52 @@ namespace CoreSharp.NHibernate
             _eventPublisher.Publish(new MappingsBuiltEvent(_configuration, mappings));
 
             return mappings;
+        }
+
+        private void AddOverride(Type type, Action<object> action)
+        {
+            var field = typeof(AutoPersistenceModel).GetField("inlineOverrides",
+                BindingFlags.Instance | BindingFlags.NonPublic);
+            var inlineOverrides = (List<InlineOverride>)field.GetValue(this);
+
+            inlineOverrides.Add(new InlineOverride(type, action));
+        }
+
+        private static bool IsAutomappingForType(object o, Type entityType)
+        {
+            var autoMappingType = ReflectionHelper.AutomappingTypeForEntityType(entityType);
+
+            return o.GetType().IsAssignableFrom(autoMappingType);
+        }
+
+        /// <summary>
+        /// Adds an IAutoMappingOverride reflectively
+        /// </summary>
+        /// <param name="overrideType">Override type, expected to be an IAutoMappingOverride</param>
+        public new void Override(Type overrideType)
+        {
+            var overrideInterfaces = overrideType.GetInterfaces().Where(x => x.IsAutoMappingOverrideType()).ToList();
+
+            foreach (var overrideInterface in overrideInterfaces)
+            {
+                var entityType = overrideInterface.GetGenericArguments().First();
+
+                AddOverride(entityType, instance =>
+                {
+                    if (!IsAutomappingForType(instance, entityType)) return;
+
+                    var overrideInstance = _container.GetInstance(overrideInterface);
+
+                    var overrideHelperMethod = typeof(AutoPersistenceModel)
+                        .GetMethod("OverrideHelper", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                    if (overrideHelperMethod == null) return;
+
+                    overrideHelperMethod
+                        .MakeGenericMethod(entityType)
+                        .Invoke(this, new[] { instance, overrideInstance });
+                });
+            }
         }
     }
 }
