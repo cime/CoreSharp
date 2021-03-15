d8757b0ebb31159217d261f2af6aeb5f4ec1c75a
jernejr <jernej.rozac@actual-it.si>
2021-03-15 11:42:18 +0100

    Grpc dictionary type channel support.

---
+++ CoreSharp.Cqrs.Tests/Grpc/Samples/Models/Application.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreSharp.Cqrs.Tests.Grpc.Samples.Models
+{
+    public class Application
+    {
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+    }
+}
